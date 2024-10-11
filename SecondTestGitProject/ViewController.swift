0975936db99d5d24dbdbc5a9f11b909970828aa0
soaringman <sargonmeister@gmail.com>
2024-10-11 13:20:45 +0300

    Initial Commit

---
+++ SecondTestGitProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SecondTestGitProject
+//
+//  Created by Алексей Гуляев on 11.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
